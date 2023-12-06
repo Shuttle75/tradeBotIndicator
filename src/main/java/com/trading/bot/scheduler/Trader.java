fc03d79f3593c1e13d8484d5ff02e0f9428c3993
Oleksii Osadchyi <alexey.osadchiy@gmail.com>
2023-12-06 08:13:55 +0200

    First

--- src/main/java/com/trading/bot/scheduler/Trader.java
+++ src/main/java/com/trading/bot/scheduler/Trader.java
@@ -2,9 +2,7 @@ package com.trading.bot.scheduler;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.CurrencyPair;
-import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.MarketOrder;
-import org.knowm.xchange.kucoin.dto.request.OrderCreateApiRequest;
 import org.knowm.xchange.kucoin.dto.response.KucoinKline;
 import org.knowm.xchange.service.trade.TradeService;
 import org.slf4j.Logger;
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBarSeries;
 import org.ta4j.core.indicators.EMAIndicator;
-import org.ta4j.core.indicators.MACDIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 
 import javax.annotation.PostConstruct;
@@ -34,14 +31,11 @@ import static org.knowm.xchange.dto.Order.OrderType.*;
 public class Trader {
     protected final Logger logger = LoggerFactory.getLogger(getClass().getName());
     private final Exchange exchange;
-    private boolean purchased;
+    private boolean purchased = true;
     private BigDecimal firstPrice;
     private final BarSeries barSeries;
-    private final MACDIndicator macdLine;
-    private final EMAIndicator signalLine;
     private final EMAIndicator emaIndicator50;
     private final EMAIndicator emaIndicator200;
-    private float macdHistogramValue;
 
     @Value("${trader.buylimit}")
     public float tradeLimit;
@@ -53,8 +47,6 @@ public class Trader {
     public Trader(Exchange exchange) {
         this.exchange = exchange;
         barSeries = new BaseBarSeries();
-        macdLine = new MACDIndicator(new ClosePriceIndicator(barSeries));
-        signalLine = new EMAIndicator(macdLine,9);
         emaIndicator50 = new EMAIndicator(new ClosePriceIndicator(barSeries),50);
         emaIndicator200 = new EMAIndicator(new ClosePriceIndicator(barSeries),200);
     }
@@ -81,7 +73,7 @@ public class Trader {
 
         if (!purchased && emaIndicator50Value > emaIndicator200Value) {
             TradeService tradeService = exchange.getTradeService();
-            MarketOrder marketOrder = new MarketOrder(BID, BigDecimal.valueOf(1000), CurrencyPair.BTC_USDT);
+            MarketOrder marketOrder = new MarketOrder(BID, BigDecimal.valueOf(1), CurrencyPair.BTC_USDT);
             String response = tradeService.placeMarketOrder(marketOrder);
 
             firstPrice = lastKline.getClose();
@@ -92,7 +84,7 @@ public class Trader {
 
         if (purchased &&  emaIndicator200Value > emaIndicator50Value) {
             TradeService tradeService = exchange.getTradeService();
-            MarketOrder marketOrder = new MarketOrder(ASK, BigDecimal.valueOf(1000), CurrencyPair.BTC_USDT);
+            MarketOrder marketOrder = new MarketOrder(ASK, BigDecimal.valueOf(1), CurrencyPair.BTC_USDT);
             String response = tradeService.placeMarketOrder(marketOrder);
 
             curAccount = curAccount.multiply(lastKline.getClose()).divide(firstPrice, 2, RoundingMode.HALF_UP);
